b145fe3a9656426c97d08ac0a3797a622bb34286
valeria0309 <42956604+valeria0309@users.noreply.github.com>
2020-03-26 22:10:30 +0300

    Update EmployeeDao.java

--- src/main/java/com/efimchick/ifmo/web/jdbc/dao/EmployeeDao.java
+++ src/main/java/com/efimchick/ifmo/web/jdbc/dao/EmployeeDao.java
@@ -2,13 +2,12 @@ package com.efimchick.ifmo.web.jdbc.dao;
 
 import java.math.BigInteger;
 import java.util.List;
-
 import com.efimchick.ifmo.web.jdbc.domain.Department;
 import com.efimchick.ifmo.web.jdbc.domain.Employee;
 
 public interface EmployeeDao extends Dao<Employee, BigInteger> {
-    List<Employee> getByDepartment(Department department) throws SQLException;
-    List<Employee> getByManager(Employee employee) throws SQLException;
+    List<Employee> getByDepartment(Department department);
+    List<Employee> getByManager(Employee employee);
 
 }
 
